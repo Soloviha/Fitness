94ad7da353dc23f6fd97fc11de9cd1bd2640522e
Katerina <katerina_mart131@mail.ru>
2024-12-06 13:50:16 +0300

    op

--- client/src/components/pages/TreningPage.tsx
+++ client/src/components/pages/TreningPage.tsx
@@ -1,93 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../providers/redux/hooks';
-import { getAllWorkouts } from '../../providers/slice/workout/WorkoutThunk';
+import React from 'react';
 
-export default  function TreningPage() {
-  const trenings = useAppSelector((state) => state.workout.workouts); // Получаем массив упражнений из Redux-стора
-  const dispatch = useAppDispatch();
-  const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isFinished, setIsFinished] = useState(false);
-  const [timeRemaining, setTimeRemaining] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
 
-  useEffect(() => {
-    void dispatch(getAllWorkouts());
-    setIsLoading(false);
-  }, [dispatch]);
+export default function TreningPage(): React.JSX.Element {
 
-  useEffect(() => {
-    if (currentExerciseIndex < trenings.length) {
-      const currentExercise = trenings[currentExerciseIndex];
-
-      // Показываем информацию об упражнении
-      console.log(`Упражнение: ${currentExercise.exOne}`);
-      console.log(`Описание: ${currentExercise.descOne}`);
-      console.log(`Время: 10 секунд`);
-
-      // Запускаем обратный отсчет времени
-      setTimeRemaining(5000);
-      const interval = setInterval(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
-      }, 1000);
-      setIntervalId(interval);
-
-      // Ждем указанное время для выполнения упражнения
-      setTimeout(() => {
-        console.log(`Отдых: 10 секунд`);
-
-        // Ждем указанное время для отдыха
-        setTimeout(() => {
-          // Повторяем упражнение, если необходимо
-          for (let i = 1; i < 15; i++) {
-            console.log(`Повторение ${i + 1} из 15`);
-
-            // Ждем указанное время для выполнения упражнения
-            setTimeout(() => {
-              // Ждем указанное время для отдыха
-              setTimeout(() => {
-                setCurrentExerciseIndex((prevIndex) => prevIndex + 1);
-              }, 10 * 1000);
-            }, 10 * 1000);
-          }
-
-          setCurrentExerciseIndex((prevIndex) => prevIndex + 1);
-        }, 10 * 1000);
-      }, 10 * 1000);
-    } else {
-      setIsFinished(true);
-      console.log('Тренировка завершена!');
-    }
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [currentExerciseIndex, trenings]);
-
-  useEffect(() => {
-    if (timeRemaining === 0) {
-      clearInterval(intervalId);
-    }
-  }, [timeRemaining, intervalId]);
-
-  if (isLoading) {
-    return <div>Загрузка...</div>;
-  }
-
-  if (isFinished) {
-    return <div>Тренировка завершена!</div>;
-  }
 
   return (
-    <div>
-      {trenings.length > 0 && (
-        <div>
-          <h2>{trenings[currentExerciseIndex].exOne}</h2>
-          <p>{trenings[currentExerciseIndex].descOne}</p>
-          <p>Время: {timeRemaining} секунд</p>
-        </div>
-      )}
-    </div>
+
   );
 }
 
--- client/src/components/pages/WorkoutPage.tsx
+++ client/src/components/pages/WorkoutPage.tsx
@@ -1,25 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../providers/redux/hooks';
 import { getAllWorkouts } from '../../providers/slice/workout/WorkoutThunk';
 import WorkoutCard from '../ui/WorkoutCard';
+// import Timer from '../ui/Timer'; // Предполагается, что этот компонент отображает обратный отсчет
 
 export default function WorkoutPage(): React.JSX.Element {
   const workouts = useAppSelector((state) => state.workout.workouts);
   const dispatch = useAppDispatch();
-  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [timerDuration, setTimerDuration] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
     void dispatch(getAllWorkouts());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (currentIndex < workouts.length) {
+      const workout = workouts[currentIndex];
+      setTimerDuration(3000); // Устанавливаем длительность таймера
+
+      const workoutTime =3000; 
+      const pauseTime = 1000; 
+
+      const totalDuration = workoutTime + pauseTime;
+
+      const timer = setTimeout(() => {
+        setCurrentIndex(currentIndex + 1);
+      }, totalDuration);
+
+      return () => clearTimeout(timer);
+    } else {
+      setIsFinished(true);
+    }
+  }, [currentIndex, workouts]);
+
+  if (isFinished) {
+    return <div>Все тренировки завершены!</div>;
+  }
+
   return (
     <div className="container">
       <div className="row">
-        {workouts.map((workout) => (
-          <div className="col-12" key={workout.id}>
-            <WorkoutCard workout={workout}/>
+        {currentIndex < workouts.length && (
+          <div className="col-12">
+            <WorkoutCard workout={workouts[currentIndex]} />
+            {/* <Timer duration={timerDuration} /> Показываем таймер для текущего workout */}
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
