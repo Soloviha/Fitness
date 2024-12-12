6a15aa81db63a77d8a4cc9944c42a39501c44ec1
MironovAndrey-1407 <mironov.andrey1407@gmail.com>
2024-12-12 09:34:57 +0300

    Chat

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from './ui/NavBar';
 import ChatBot from './ui/ChatBot';
-import FootBar from './ui/FootBar';
+// import FootBar from './ui/FootBar';
 
 export default function Layout(): React.JSX.Element {
   return (
